633cacb06080eef735c2d5a6eb24b2adabd5e58f
ipiv <hello.ipiv@gmail.com>
2021-03-18 20:48:12 +0100

    dir

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import formidable from "formidable";
-import path from 'path'
 
 export const config = {
   api: {
@@ -10,11 +9,9 @@ export const config = {
 
 export default (req, res) => {
   if (req.method === 'POST') {
-    console.log(process.cwd())
-    console.log(path.join(process.cwd(), 'uploads'))
     const form = new formidable.IncomingForm({
       // multiples: true,
-      uploadDir: `uploads`,
+      // uploadDir: `uploads`,
       keepExtensions: true
     });
     form.parse(req, function(err, fields, files) {
