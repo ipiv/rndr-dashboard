0ef028e285998b7ef37c68dad1d236471c452438
ipiv <hello.ipiv@gmail.com>
2021-03-21 23:52:40 +0100

    firefox new Date doesn't accept datestrings with -

--- helpers/parseDate.js
+++ helpers/parseDate.js
@@ -3,8 +3,7 @@ export const parseDate = (dateString) => {
   // Unable to parse date without AM/PM data...
   const [date, time] = dateString.split(' ');
   const dateParts = date.split('-');
-
   return new Date(
-    `${dateParts[2]}-${dateParts[1]}-${dateParts[0]} ${time} AM`
+    `${dateParts[2]}/${dateParts[1]}/${dateParts[0]} ${time} AM`
   ).getTime();
 };
