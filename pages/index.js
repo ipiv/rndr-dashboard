064b85d58ba3840a6b48ce8d1a4c5cdd561c50b6
ipiv <hello.ipiv@gmail.com>
2021-03-22 00:25:56 +0100

    footer links styling

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,12 @@ export default function Home() {
       <main className={styles.main}>
         <div>
           <h1 className={styles.title}>
-            Welcome to <a href="https://rendertoken.com/" target="_blank">RNDR</a> dashboard!*
+            Welcome to <a href="https://rendertoken.com/" target="_blank" rel="noopener noreferrer">RNDR</a> dashboard!*
           </h1>
 
           <p className={styles.description}>
             Personal project! {' '}
-            <code className={styles.code}>*not affiliated with <a href="https://home.otoy.com/" target="_blank">OTOY</a></code>
+            <code className={styles.code}>*not affiliated with <a href="https://home.otoy.com/" target="_blank" rel="noopener noreferrer">OTOY</a></code>
           </p>
         </div>
         { logData ? 
@@ -35,16 +35,15 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <div>☕Eth: 0x5251dc3700164ed4a1Af9d3f86e8259d1eBea502</div>
-        <a
-          href="https://twitter.com/ipivDev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>Made with 💙 by ipiv</p>
-        </a>
-          {
-            <a href="https://github.com/ipiv/rndr-dashboard" target="_blank"><img src="/github.svg" alt="Github Logo" className={styles.logo}/></a>
-          }
+        <div className={styles.footerlinks}>
+          <a href="https://twitter.com/ipivDev" target="_blank" rel="noopener noreferrer">
+            <p>Made with 💙 by ipiv</p>
+          </a>
+          <a href="https://github.com/ipiv/rndr-dashboard" target="_blank" rel="noopener noreferrer" className={styles.github}>
+            <img src="/github.svg" alt="Github Logo"/>
+            <p>Github</p>
+          </a>
+        </div>
       </footer>
     </div>
   )
