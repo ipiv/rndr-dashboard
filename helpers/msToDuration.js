dc2fa0fa04258a2f060e4bd4d2c373888c8b6855
ipiv <hello.ipiv@gmail.com>
2021-03-24 19:34:51 +0100

    invalid ms input to msToDuration?

--- helpers/msToDuration.js
+++ helpers/msToDuration.js
@@ -1,10 +1,12 @@
 export const msToDuration = (ms) => {
+  if (!ms) return 'Invalid'
   const days = Math.floor(ms / (1000 * 3600 * 24));
   ms -= days * 1000 * 3600 * 24;
   try {
     const time = new Date(ms).toISOString().substr(11, 8);
     return `${days ? days + ' days, ' : ''}${time}`;
   } catch(err) {
+    err.Message += '\nTried to convert: '+ms
     console.trace('Tried to convert: ', ms)
     throw err
   }
